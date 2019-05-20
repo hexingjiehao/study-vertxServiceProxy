a68a662c36390fbd7712c522061f6448b19540f1
jxiong <jxiong@qjnext.com>
2019-05-20 12:34:55 +0800

    学习vertx-circiut-breaker，熔断器的设置和回调

--- src/test/java/com/xiongjie/dicovery/ServicePublicTest.java
+++ src/test/java/com/xiongjie/dicovery/ServicePublicTest.java
@@ -1,7 +1,8 @@
 package com.xiongjie.dicovery;
 
 import com.xiongjie.discovery.ServicePublic;
-import com.xiongjie.eb.MyproxyVerticle;
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
@@ -39,8 +40,8 @@ public class ServicePublicTest {
             //将http发布为服务
             ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
             discovery.publish(HttpEndpoint.createRecord("world", "localhost", 8883, "/"), publish -> {
-                Record record=publish.result();
-                System.out.println("http service 发布成功! "+record.getName());
+                Record record = publish.result();
+                System.out.println("http service 发布成功! " + record.getName());
             });
         }));
     }
@@ -74,6 +75,128 @@ public class ServicePublicTest {
                     async.complete();
                 });
             });
+        });
+    }
+
+    /**
+     * 测试熔断器1,在具体的操作中设置回退，breaker.executeWithFallback
+     */
+    @Test
+    public void circuitBreakerTest(TestContext context) {
+        Async async = context.async();
+        ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+
+        //创建熔断器对象，随意指定名字
+        CircuitBreaker breaker = CircuitBreaker.create("my-circuit-breaker", vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(5)
+                        .setTimeout(2000)
+                        .setFallbackOnFailure(true)
+                        .setResetTimeout(10000)
+        );
+
+        breaker.executeWithFallback(
+                future -> {
+                    discovery.getRecord(new JsonObject().put("name", "world"), ar -> {
+                        ServiceReference reference = discovery.getReference(ar.result());
+                        HttpClient httpClient = reference.getAs(HttpClient.class);
+                        httpClient.getNow("/", response -> {
+                            response.handler(body -> {
+                                future.complete(body.toString());
+                                reference.release();
+                            });
+                        });
+                    });
+                }, v -> {
+                    //服务熔断时进行的操作,当world服务没有发布时
+                    return "world服务未发现！";
+                })
+                .setHandler(ar -> {
+                    System.out.println(ar.result());
+                    async.complete();
+                });
+
+    }
+
+
+    /**
+     * 测试熔断器2，在创建熔断器时设置反馈方法
+     */
+    @Test
+    public void circuitBreakerTest2(TestContext context) {
+        Async async = context.async();
+        ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+
+        //创建熔断器对象，随意指定名字
+        CircuitBreaker breaker = CircuitBreaker.create("my-circuit-breaker", vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(5)
+                        .setTimeout(2000)
+                        .setFallbackOnFailure(true)
+                        .setResetTimeout(10000)
+        ).fallback(v -> {
+            return "world服务未发现！";
+        });
+
+        breaker.execute(future -> {
+            discovery.getRecord(new JsonObject().put("name", "world"), ar -> {
+                ServiceReference reference = discovery.getReference(ar.result());
+                HttpClient httpClient = reference.getAs(HttpClient.class);
+                httpClient.getNow("/", response -> {
+                    response.handler(body -> {
+                        future.complete(body.toString());
+                        reference.release();
+                    });
+                });
+            });
+        }).setHandler(ar -> {
+            System.out.println(ar.result());
+            async.complete();
+        });
+    }
+
+    /**
+     * 测试熔断器3，熔断器的重试次数和状态改变的操作,以及重试策略
+     */
+    @Test
+    public void circuitBreakerTest3(TestContext context) {
+        Async async = context.async();
+        ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+
+        //创建熔断器对象，随意指定名字
+        CircuitBreaker breaker = CircuitBreaker.create("my-circuit-breaker", vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(5)
+                        .setTimeout(2000)
+                        .setFallbackOnFailure(true)
+                        .setResetTimeout(1000)
+        ).openHandler(v1 ->{
+            System.out.println("服务没发现，熔断器开启");
+        }).closeHandler(v2 ->{
+            System.out.println("服务正常工作，熔断器关闭");
+        }).halfOpenHandler(v3 ->{
+            System.out.println("服务重试中，熔断器半开");
+        }).retryPolicy(retryCount ->{
+            return retryCount*1L;
+        })
+        .fallback(v -> {
+            return "world服务未发现！";
+        });
+
+        breaker.execute(future -> {
+            discovery.getRecord(new JsonObject().put("name", "world"), ar -> {
+                ServiceReference reference = discovery.getReference(ar.result());
+                HttpClient httpClient = reference.getAs(HttpClient.class);
+                httpClient.getNow("/", response -> {
+                    response.handler(body -> {
+                        future.complete(body.toString());
+                        reference.release();
+                    });
+                });
+            });
+        }).setHandler(ar -> {
+            System.out.println(ar.result());
+            async.complete();
         });
     }
 }
